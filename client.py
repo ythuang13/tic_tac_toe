818b2fe501ab3fb27704bd9d604b5d38bcefcd87
ythuang13 <ythuang13@users.noreply.github.com>
2021-03-10 23:51:05 -0800

    include .gitignore add game over screen, will now pause when a game ends and show who won fix your turn text blur game board when win align the O and X

--- client.py
+++ client.py
@@ -4,7 +4,8 @@ import pygame
 import sys
 
 # server socket settings
-HOST = "54.241.79.181"
+# 54.241.79.181
+HOST = "localhost"
 PORT = 5555
 
 # network, connection with server
@@ -29,6 +30,7 @@ grid = pygame.image.load("asset\\grid.png")
 # font
 base_font = pygame.font.Font(None, 45)
 turn_font = pygame.font.Font(None, 35)
+game_over_font = pygame.font.Font(None, 170)
 
 # clear and reset
 btn_clear = Button(400, 10, 80, 40, (180, 180, 180), "clear")
@@ -36,6 +38,26 @@ btn_reset = Button(400, 60, 80, 40, (180, 180, 180), "reset")
 
 # render your turn text
 turn_surface = turn_font.render("Your Turn", True, (255, 255, 255))
+
+# render game over text
+x_won = game_over_font.render("X Won!", True, (150, 20, 180))
+o_won = game_over_font.render("O Won!", True, (150, 20, 180))
+tie_game = game_over_font.render("Tie!", True, (150, 20, 180))
+
+
+def blur_surface(surface, amt=1):
+    """
+    Blur the given surface by the given "amount"
+    :param surface: surface to blur
+    :param amt: amount to blur
+    :return: return blurred surface
+    """
+    scale = 1.0 / float(amt)
+    surf_size = surface.get_size()
+    scale_size = (int(surf_size[0] * scale), int(surf_size[1] * scale))
+    surf = pygame.transform.smoothscale(surface, scale_size)
+    surf = pygame.transform.smoothscale(surf, surf_size)
+    return surf
 
 
 def draw_and_update(g, player_number):
@@ -49,7 +71,7 @@ def draw_and_update(g, player_number):
     btn_reset.draw(screen, (255, 255, 255))
 
     # draw your turn text
-    if g.round % 2 + 1 == player_number:
+    if g.round % 2 != player_number % 2:
         screen.blit(turn_surface, (180, 90))
 
     # draw score
@@ -62,11 +84,43 @@ def draw_and_update(g, player_number):
     for i in range(3):
         for j in range(3):
             if g.board[i][j] == "O":
-                board_surface.blit(o_img, (j * 166, i * 166))
+                board_surface.blit(o_img, (j * 166 + 3, i * 166 + 6))
             elif g.board[i][j] == "X":
-                board_surface.blit(x_img, (j * 166, i * 166))
+                board_surface.blit(x_img, (j * 166 + 3, i * 166 + 6))
 
-    screen.blit(board_surface, (0, 120))
+    # draw board on screen, blur if win_move
+    if g.win_move:
+        screen.blit(blur_surface(board_surface, 10), (0, 120))
+    else:
+        screen.blit(board_surface, (0, 120))
+
+    # draw game over text and win move
+    if g.win_move:
+        if g.win_move == 1:
+            screen.fill((255, 0, 0), rect=(5, 120 + 160 * 0 + 83, 455, 8))
+        elif g.win_move == 2:
+            screen.fill((255, 0, 0), rect=(5, 120 + 160 * 1 + 83, 455, 8))
+        elif g.win_move == 3:
+            screen.fill((255, 0, 0), rect=(5, 120 + 160 * 2 + 83, 455, 8))
+        elif g.win_move == 4:
+            screen.fill((255, 0, 0), rect=(160 * 0 + 83, 120 + 5, 8, 455))
+        elif g.win_move == 5:
+            screen.fill((255, 0, 0), rect=(160 * 1 + 83, 120 + 5, 8, 455))
+        elif g.win_move == 6:
+            screen.fill((255, 0, 0), rect=(160 * 2 + 83, 120 + 5, 8, 455))
+        elif g.win_move == 7:
+            pygame.draw.polygon(screen, (255, 0, 0),
+                                [(3, 127), (7, 123), (497, 613), (493, 617)])
+        elif g.win_move == 8:
+            pygame.draw.polygon(screen, (255, 0, 0),
+                                [(493, 123), (497, 127), (7, 617), (3, 613)])
+
+    if g.winner == "O":
+        screen.blit(o_won, (50, 310))
+    elif g.winner == "X":
+        screen.blit(x_won, (50, 310))
+
+    # update pygame display
     pygame.display.flip()
 
 
--- game.py
+++ game.py
@@ -5,6 +5,8 @@ class TicTacToe:
         self._board = [["", "", ""] for _ in range(3)]
         self._player1_score = self._player2_score = 0
         self._current_player = None
+        self._win_move = None
+        self._winner = None
 
     def __str__(self):
         board = "\n"
@@ -28,9 +30,6 @@ class TicTacToe:
 
     @property
     def score(self) -> tuple:
-        """
-        :return: score of both player in a tuple
-        """
         return self._player1_score, self._player2_score
 
     @property
@@ -40,6 +39,14 @@ class TicTacToe:
     @property
     def board(self) -> list:
         return self._board
+
+    @property
+    def win_move(self) -> int:
+        return self._win_move
+
+    @property
+    def winner(self) -> str:
+        return self._winner
 
     def play_move(self, x: int, y: int, char: str):
         if 0 <= x <= 2 and 0 <= y <= 2:
@@ -56,10 +63,12 @@ class TicTacToe:
 
     def clear_board(self):
         self._board = [["", "", ""] for _ in range(3)]
-        self._round = 0
+        self._winner = None
+        self._win_move = None
 
     def full_reset(self):
         self.clear_board()
+        self._round = 0
         self._player1_score = self._player2_score = 0
 
     def check_win(self):
@@ -67,52 +76,68 @@ class TicTacToe:
         is_winner = False
         if b[0][0] == b[0][1] == b[0][2] == "O":
             self._player2_score += 1
-            is_winner = True
+            self._winner = "O"
+            self._win_move = 1
         elif b[0][0] == b[0][1] == b[0][2] == "X":
             self._player1_score += 1
-            is_winner = True
+            self._winner = "X"
+            self._win_move = 1
         elif b[1][0] == b[1][1] == b[1][2] == "O":
             self._player2_score += 1
-            is_winner = True
+            self._winner = "O"
+            self._win_move = 2
         elif b[1][0] == b[1][1] == b[1][2] == "X":
             self._player1_score += 1
-            is_winner = True
+            self._winner = "X"
+            self._win_move = 2
         elif b[2][0] == b[2][1] == b[2][2] == "O":
             self._player2_score += 1
-            is_winner = True
+            self._winner = "O"
+            self._win_move = 3
         elif b[2][0] == b[2][1] == b[2][2] == "X":
             self._player1_score += 1
-            is_winner = True
+            self._winner = "X"
+            self._win_move = 3
         elif b[0][0] == b[1][0] == b[2][0] == "O":
             self._player2_score += 1
-            is_winner = True
+            self._winner = "O"
+            self._win_move = 4
         elif b[0][0] == b[1][0] == b[2][0] == "X":
             self._player1_score += 1
-            is_winner = True
+            self._winner = "X"
+            self._win_move = 4
         elif b[0][1] == b[1][1] == b[2][1] == "O":
             self._player2_score += 1
-            is_winner = True
+            self._winner = "O"
+            self._win_move = 5
         elif b[0][1] == b[1][1] == b[2][1] == "X":
             self._player1_score += 1
-            is_winner = True
+            self._winner = "X"
+            self._win_move = 5
         elif b[0][2] == b[1][2] == b[2][2] == "O":
             self._player2_score += 1
-            is_winner = True
+            self._winner = "O"
+            self._win_move = 6
         elif b[0][2] == b[1][2] == b[2][2] == "X":
             self._player1_score += 1
-            is_winner = True
+            self._winner = "X"
+            self._win_move = 6
         elif b[0][0] == b[1][1] == b[2][2] == "O":
             self._player2_score += 1
-            is_winner = True
+            self._winner = "O"
+            self._win_move = 7
         elif b[0][0] == b[1][1] == b[2][2] == "X":
             self._player1_score += 1
-            is_winner = True
+            self._winner = "X"
+            self._win_move = 7
         elif b[0][2] == b[1][1] == b[2][0] == "O":
             self._player2_score += 1
-            is_winner = True
+            self._winner = "O"
+            self._win_move = 8
         elif b[0][2] == b[1][1] == b[2][0] == "X":
             self._player1_score += 1
-            is_winner = True
+            self._winner = "X"
+            self._win_move = 8
 
         if is_winner:
             self.clear_board()
